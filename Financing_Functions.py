aa766280bcc9a0c93728df1d90e8948037df4ea1
christiankno <47571346+christiankno@users.noreply.github.com>
2020-02-05 16:46:26 +0100

    added parse_descriptions and parse_categories functions
    these new functions are not yet tested. they are supposed to modify the description to contain only the necessary keywords and use this modified description to assign a category to it.
    Began creation of plot function. not ready yet

--- Financing_Functions.py
+++ Financing_Functions.py
@@ -1,5 +1,6 @@
 import os
 import pandas as pd
+import matplotlib as plt
 
 def get_data(name,workspace=r'C:\Users\chris\Documents\Unimportant\\', from_original=0):
     print('Getting Data...')
@@ -22,5 +23,27 @@ def get_data(name,workspace=r'C:\Users\chris\Documents\Unimportant\\', from_orig
                          dayfirst=True, infer_datetime_format=True, parse_dates=['Book_Date', 'Pay_Date'])
     else: df = pd.read_pickle(pklpath)
     df=df.fillna(value=0)
+    df=df.drop(columns=['Pay_Date', 'Auftraggeberkonto', 'BLZ_Auftraggeberkonto', 'IBAN Auftraggeberkonto'])
     print('DONE')
     return df
+
+def parse_descriptions(dataframe, keywords):
+    print('Parsing Descriptions...')
+    df=dataframe
+    for key in keywords:
+        df.loc[df['Description'].contains(key), 'Description']=key
+    print('DONE')
+    return df
+        
+def parse_categories(dataframe, categories_dict):
+    print('Parsing Categories...')
+    df=dataframe
+    for group in categories_dict.keys():
+        df.loc[df['Description'].isin(categories_dict[group]), 'Category'] = group
+    print('DONE')
+    return df
+
+def plot_pie(dataframe, group_by='Category', colormap='hsv'):
+    df_group_sum=dataframe.group_by(by=group_by).sum()
+    df_group_sum.plot.pie(y='Amount', cmap=colormap, labels=df.index())
+       
